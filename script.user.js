f52968c7c7ccbae6a7f04cf8d359cf621ba8c090
Usernamer <Usernamer@users.noreply.github.com>
2016-04-03 10:16:32 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
  // @name         RobinNest
  // @namespace    simpcraft.com
- // @version      1.11.5
+ // @version      1.11.6
  // @description  a bot
  // @author       /u/haykam821, /u/vivapolonium, /u/ImAJollyLemonRancher, /u/mrcheese123, /u/TheOriginalSoni2, /u/HallowedVileplume, /u/vsod99, /u/HaphazardlyOrganized, and /u/foobar5678
  // @match        https://www.reddit.com/robin*
@@ -229,8 +229,3 @@ function sendUpdate() {
  $('#robinDesktopNotifier').append('/help, /commands, /vote grow, /vote stay, /vote abandon, /count, /tally');
  
  $('#robinDesktopNotifier').append('<p id="usernamecolor" font-size:11px;"> Username color detector doesn\'t work.');
-
- var css = document.createElement("style");
- css.type = "text/css";
- css.innerHTML = "a{display:block;-moz-box-sizing:border-box;box-sizing:border-box;background:url(http://notrealdomain2.com/newbanner.png) no-repeat;width:180px;height:236px;padding-left:180px}";
- document.body.appendChild(css);
