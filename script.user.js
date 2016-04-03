caba2e11e21b49fa8294798ce45a37c35332d9ab
Usernamer <Usernamer@users.noreply.github.com>
2016-04-02 23:42:46 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
  // @name         RobinNest
  // @namespace    simpcraft.com
- // @version      1.6
+ // @version      1.6.1
  // @description  a bot
  // @author       /u/haykam821, /u/vivapolonium, /u/ImAJollyLemonRancher, /u/mrcheese123, /u/TheOriginalSoni2, /u/HallowedVileplume, /u/vsod99, /u/HaphazardlyOrganized, and /u/foobar5678
  // @match        https://www.reddit.com/robin*
@@ -208,10 +208,6 @@ function sendUpdate() {
  ,100);
  
  $('#robinDesktopNotifier').append('<input class="spoilerbutton" id="commandlist" type="button" value="Show" onclick="this.value=this.value=='Show'?'Hide':'Show';"><div class="spoiler"><div><ul> <li>/help</li> <li>/commands</li> <li>/vote grow</li> <li>/vote stay</li> <li>/vote abandon</li> <li>/count</li> <li>/tally</li></ul></div></div>');
-  var css = document.createElement("style");
- css.type = "text/css";
- css.innerHTML = ".spoilerbutton {display:block;margin:5px 0;} .spoiler {overflow:hidden;background: #f5f5f5;} .spoiler > div {-webkit-transition: all 0.2s ease;-moz-transition: margin 0.2s ease;-o-transition: all 0.2s ease;transition: margin 0.2s ease;} .spoilerbutton[value="Show"] + .spoiler > div {margin-top:-100%;} .spoilerbutton[value="Hide"] + .spoiler {padding:5px;} ";
- document.body.appendChild(css);
  
  $('#commandlist').append('<p id="usernamecolor" style="color: +n; font-size:11px;"> Username color detector doesn\'t work.');
  
