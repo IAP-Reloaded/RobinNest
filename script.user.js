b3c4f15f9b032231827457b317c2a501a9fd1ec2
Usernamer <Usernamer@users.noreply.github.com>
2016-04-03 00:08:24 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
  // @name         RobinNest
  // @namespace    simpcraft.com
- // @version      1.7
+ // @version      1.7.1
  // @description  a bot
  // @author       /u/haykam821, /u/vivapolonium, /u/ImAJollyLemonRancher, /u/mrcheese123, /u/TheOriginalSoni2, /u/HallowedVileplume, /u/vsod99, /u/HaphazardlyOrganized, and /u/foobar5678
  // @match        https://www.reddit.com/robin*
@@ -195,8 +195,8 @@ function sendUpdate() {
  var target = document.querySelector('#robinChatMessageList');
  var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        var msg = $(mutation.addedNodes[0].lastElementChild).text();
-        if(msg.includes('haykam821')
+        var author = $(mutation.addedNodes[0].children[1]).text();
+        if(author.includes('haykam821')
 )
         ) {
             mutation.addedNodes[0].style.color = "gold";
@@ -230,4 +230,4 @@ function sendUpdate() {
  
  $('#robinDesktopNotifier').append('<p id="usernamecolor" font-size:11px;"> Username color detector doesn\'t work.');
  
- $('.content').append('<p id=robinsnest">I will add a new interface here. - Running v1.6.3 or so.</p>');
+ $('.content').append('<p id=robinsnest">I will add a new interface here. - Running v1.7.1 or so.</p>');
