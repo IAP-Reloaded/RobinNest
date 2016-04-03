36586ad0fd5d47ebfdb7765eadf51a0c3bfeae8b
Usernamer <Usernamer@users.noreply.github.com>
2016-04-03 00:16:28 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
  // @name         RobinNest
  // @namespace    simpcraft.com
- // @version      1.7.3
+ // @version      1.7.4
  // @description  a bot
  // @author       /u/haykam821, /u/vivapolonium, /u/ImAJollyLemonRancher, /u/mrcheese123, /u/TheOriginalSoni2, /u/HallowedVileplume, /u/vsod99, /u/HaphazardlyOrganized, and /u/foobar5678
  // @match        https://www.reddit.com/robin*
@@ -195,7 +195,7 @@ function sendUpdate() {
  var target = document.querySelector('#robinChatMessageList');
  var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        var author = $(mutation.addedNodes[0].firstElementChild.nextElementSibling).text();
+        var author = $(mutation.addedNodes[0].firstElementChild.children[1]).text();
         if(author.includes('haykam821')
 )
         ) {
