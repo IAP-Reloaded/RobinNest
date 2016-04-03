f7e388957d121d59e95ca486903df64200edaed9
Usernamer <Usernamer@users.noreply.github.com>
2016-04-02 23:28:55 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -8,22 +8,6 @@
  // @grant        none
  // ==/UserScript==
  
- setInterval(sendUpdate, 5 * 60 * 1000);
-sendUpdate();
-
-function sendUpdate() {
-    $.get("https://www.reddit.com/robin")
-        .then(function (body) {
-            var numMembers = body.match("robin_user_list.*?\\[(.*?)\\]")[1].split('"name":').length + 1,
-                roomName = body.match('robin_room_name":.*?"(.*?)"')[1].substr(0, 200);
-
-            $.post("https://robin-tracker.herokuapp.com/update", {
-                "room_name": roomName,
-                "num_members": numMembers
-            });
-        });
-}
- 
  function sendMessage(message){
      $("#robinSendMessage > input[type='text']").val(message);
      $("#robinSendMessage > input[type='submit']").click();
